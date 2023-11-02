f843361db38be7d49ab879dc42eaf35651a04432
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-02 22:42:30 +0330

    spyOn test on register page completed

--- src/components/register/register.test.js
+++ src/components/register/register.test.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 import Register from "./index";
 import { act } from "react-dom/test-utils";
+import { utils } from "../helper";
 
 const getElement = (element) => {
   const elements = {
-    Email: screen.getByRole("textbox", { name: "Email" }),
-    Password: screen.getByLabelText("Password"),
+    "Email": screen.getByRole("textbox", { name: "Email" }),
+    "Password": screen.getByLabelText("Password"),
     "Confirm Password": screen.getByLabelText("Confirm Password"),
+    "Button" : screen.getByRole("button",{name:"Submit"})
   };
   if (elements[element]) return elements[element];
 };
@@ -57,5 +59,19 @@ describe("Register Page", () => {
   test("should be able to type into inputs and get value",()=>{
     changeElement("Email","amiraliazimloo123@gmail.com")
     expect(getElement("Email").value).toBe("amiraliazimloo123@gmail.com")
+  })
+
+  // spyOn => jasoosi kardan az bakhsh haye proje
+  test("button should be disabled when all inputs are empty",()=>{
+    expect(getElement("Button")).toHaveClass("btn")
+    expect(getElement("Button")).toBeDisabled()
+
+    // jest spyOn
+    // mikhaym bbinim aya isEmpty bad az ezafe shodane data ha call khahad shod ya na
+    const isEmpti = jest.spyOn(utils,"isEmpty")
+    utils.isEmpty(getElement("Email").value)
+    utils.isEmpty(getElement("Password").value)
+    utils.isEmpty(getElement("Confirm Password").value)
+    expect(isEmpti).toHaveBeenCalledTimes(3)
   })
 });
