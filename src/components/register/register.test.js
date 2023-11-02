3f0bf120d553ddf391b8bf75c9279bfc75e353ee
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-02 22:28:07 +0330

    register page testing completed

--- src/components/register/register.test.js
+++ src/components/register/register.test.js
@@ -1,18 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import Register from "./index";
+import { act } from "react-dom/test-utils";
 
-const getElement=(element)=>{
-  const elements={
-    "Email":screen.getByRole("textbox", { name: "Email"}),
-    "Password": screen.getByLabelText("Password"),
+const getElement = (element) => {
+  const elements = {
+    Email: screen.getByRole("textbox", { name: "Email" }),
+    Password: screen.getByLabelText("Password"),
     "Confirm Password": screen.getByLabelText("Confirm Password"),
-  }
-  if( elements[element])  return elements[element]
-}
-describe("Register Page", () => {
-  test("Inputs should be initially empty", () => {
+  };
+  if (elements[element]) return elements[element];
+};
 
+const changeElement = (element,value)=>{
+  // az act estefade mikonim chon dare yek state taghiir peyda mikone va yek amalkarde async mahsoob mishe
+  act(()=>{
+    userEvent.type(getElement(element),value)
+  })
+}
+
+describe("Register Page", () => {
+
+  // run this before each test
+  beforeEach(() => {
+    // Arrange
+    render(
+      <BrowserRouter>
+        <Register />
+      </BrowserRouter>
+    );
+  });
+
+  test("Inputs should be initially empty", () => {
     // SingleEllement // like one input
     //getEllemnt => vaghti safhe az aval mikhaym yek elementi ro dashte bashe
     //queryEllement => vaghti bad az yek amalkardi mikhaym yek elementi ro dashte bashe
@@ -23,13 +43,7 @@ describe("Register Page", () => {
     // queryyAll...
     // findAll...
 
-
-    // Arrange
-    render(
-    <BrowserRouter>
-    <Register/>
-    </BrowserRouter>
-    )
+    
 
     // Act
     // Change event - click event or ...
@@ -38,5 +52,10 @@ describe("Register Page", () => {
     expect(getElement("Email").value).toBe("");
     expect(getElement("Password").value).toBe("");
     expect(getElement("Confirm Password").value).toBe("");
-  });
+  }); 
+
+  test("should be able to type into inputs and get value",()=>{
+    changeElement("Email","amiraliazimloo123@gmail.com")
+    expect(getElement("Email").value).toBe("amiraliazimloo123@gmail.com")
+  })
 });
